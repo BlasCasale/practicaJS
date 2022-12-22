b39362eb60bbaa4012aac4270b47bc08b775cdbb
BlasCasale <casale.blas@live.com>
2022-12-22 16:58:39 -0300

    first commit

---
+++ js/practica1.js
@@ -0,0 +1,62 @@
+// actividad 1
+// como se hacen las variables y son valores que pueden cambiar
+
+// let nombre = "Homero"
+// let apellido = "Simpson"
+// let edad = "35"
+
+// console.log(nombre)
+// console.log(apellido)
+// console.log(edad)
+
+// actividad 2
+// como se hacen las constantes y son valores que jamas van a cambiar
+
+// const ciudad1 = "La Plata"
+// const ciudad2 = "Mar del Plata"
+// const ciudad3 = "Berisso"
+// const ciudad4 = "Ensenada"
+
+// actividad 3
+// hacer un carnet
+
+// let nombre = "Blas"
+// let apellido = "Casale"
+// let direccion = "11 bis e/ 654 y 655"
+// let pais = "Argentina"
+// let cumple = "03/10/1997"
+// const codigo = "B47U89RE243"
+// let carnet =    "Codigo: " + codigo + "."
+//                 "Nombre: " + nombre + "."
+//                 "Apellido: " + apellido + "."
+//                 "Dirección: " + direccion + "."
+//                 "Pais: " + pais + "."
+//                 "Nacimiento: " + cumple + "."
+
+// activdidad 4
+// concatenar los nombres de los integrantes de la familia simpsons
+
+// let miembro1 = prompt("Ingresa el nombre de un miembro de la familia Simpsons.")
+// let miembro2 = prompt("Ingresa el nombre de otro miembro de la familia.")
+// let miembro3 = prompt("Ingresa el nombre de otro miembro de la familia.")
+// let miembro4 = prompt("Ingresa el nombre de otro miembro de la familia.")
+// let miembro5 = prompt("Ingresa el nombre de otro miembro de la familia.")
+// let familia =   "1) " + miembro1 + " " +
+//                 "2) " + miembro2 + " " +
+//                 "3) " + miembro3 + " " +
+//                 "4) " + miembro4 + " " +
+//                 "5) " + miembro5 + " " 
+// console.log(familia)
+// alert("Los miembros de la familia Simpsons son " + miembro1 + ", " + miembro2 + ", " + miembro3 + ", " + miembro4 + " y " + miembro5 + ".")
+
+// estaba bien a mi manera y copie la otra manera tambien
+
+// actividad 5
+// calcular los descuentos de un precio
+
+// let precio = Number(prompt("Ingresa un precio.") )
+// let descuento20 = precio - (precio * 0.2)
+// let descuento30 = precio - (precio * 0.3)
+
+// console.log(descuento20)
+// console.log(descuento30)
---
+++ js/practica3.js
@@ -0,0 +1,93 @@
+
+
+// asi se hace un calculo matematico
+
+// let n1, n2, n3, n4, resultado;
+
+// n1 = Number (prompt("Ingresa un numero.") )
+// n2 = Number (prompt("Ingresa un numero.") )
+// n3 = Number (prompt("Ingresa un numero.") )
+// n4 = Number (prompt("Ingresa un numero.") )
+// resultado = n1 + n2 + n3 + n4
+
+// alert(resultado)
+// console.log(resultado)
+
+// incrementador o decrentor
+// let vida = 4
+// vida++
+// vida--
+// console.log(vida)
+
+// let durazno = 100
+// durazno %= 7
+
+// console.log(durazno)
+
+// operador && "y"
+// tengoHambre = true
+// tengoComida = true
+
+// let comoComida = tengoHambre && tengoComida
+
+// console.log(comoComida)
+
+// operador || "o"
+
+// let uber = false
+// let micro = true
+// let tren = true 
+// let bici = false
+
+// let trabajo = uber || micro || tren || bici
+
+// console.log(trabajo)
+
+
+// operador NOT !
+// let meGustaCoger = !false
+
+// console.log(meGustaCoger)
+
+
+// operadores logicos
+
+// let n1, n2, resultado
+
+// n1 = 8
+// n2 = 1
+
+// resultado = n1 < n2
+
+// console.log(resultado) "da false"
+
+
+// algoritmo con if
+
+// let diaQueNosConocimos = 4
+// let respuesta = Number(prompt("Que dia nos conocimos mi amor?"))
+
+// if (respuesta == diaQueNosConocimos) {
+//     alert("Te acordas, te re amo tarada.") <- tiene que ir siempre primero el afirmativo, sino no funciona el algoritmo 
+// } else {
+//     alert("Ouch.")
+// }
+
+
+// primer algoritmo mas o menos 
+
+// let primeraSerie = "Bojack Horseman"
+// let pizzaPreferida = "Papa"
+// let sagaDePelis = "Harry Potter"
+// let cantanteQuemado = "Mac Miller"
+
+// primeraSerie = prompt("Cual fue nuestra primer serie juntos?")
+// pizzaPreferida = prompt("Cual es nuestra pizza estrella?")
+// sagaDePelis = prompt("Cual es la saga de pelis que mas nos va a costar ver en toda la vida?")
+// cantanteQuemado = prompt("Que cantante quemamos?")
+
+// if ( ("Bojack Horseman" == primeraSerie || "Bojack horseman" == primeraSerie || "bojack horseman" == primeraSerie) && ("Papa" == pizzaPreferida || "papa" == pizzaPreferida) && ("Harry Potter" == sagaDePelis || "Harry potter" == sagaDePelis || "harry potter" == sagaDePelis) && ("Mac Miller" == cantanteQuemado || "Mac miller" == cantanteQuemado || "mac miller" == cantanteQuemado) ) {
+//     alert("Te amo corazon de melón.")
+// } else {
+//     alert("No no, hay algo que no, es un solo un juego corazon de melón, te amo mucho.")
+// }
